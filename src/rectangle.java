3a0c83bb6ced452952c1ce0814507b67357b9c17
root <root@ip-172-31-42-237.us-east-2.compute.internal>
2018-01-31 03:35:15 +0000

    changes added

--- src/rectangle.java
+++ src/rectangle.java
@@ -1,20 +1,20 @@
-public class rectangle {
-  public int length;
-  public int width;
+    public class rectangle {
+      public int length;
+      public int width;
 
 
-  public rectangle(int length, int width) {
-    this.length = length;
-    this.width = width;
+      public rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
    
-  }
+      }
 
-  public int getArea() {
-    return length * width;
+      public int getArea() {
+        return length * width;
 
-  }
+      }
 
-  public int getPerimeter() {
-    return 2 * (length + width);
-  }
-}
+      public int getPerimeter() {
+        return 2 * (length + width);
+      }
+   }
